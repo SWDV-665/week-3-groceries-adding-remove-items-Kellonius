26f055402d45bf733ccb74e3cccf5bdc09b66245
Kellonius <beaslera83@gmail.com>
2019-03-28 21:39:56 -0500

    Made changes for adding and removing items
    Made changes for adding and removing items and added screenshots

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -12,15 +13,53 @@ export class Tab1Page {
   { Name: "Blueberries", Type: "Highbush", src: "/assets/blueberry.jpg", purchased: false },
   { Name: "Strawberries", Type: "June Bearing", src: "/assets/strawberry.jpg", purchased: false }
   ]
-  
-  toggle(item:any){
+
+  constructor(public alertController: AlertController, public toastController: ToastController) { }
+
+  toggle(item: any) {
     item.purchased = !item.purchased
   }
 
-  delete(item:any) {
+  async delete(item: any) {
+    const toast = await this.toastController.create({
+      message: "Removing item: " + item.Name + ", " + item.Type,
+      duration: 3000
+    })
+
+    toast.present();
+
     const index: number = this.items.indexOf(item);
     if (index !== -1) {
-        this.items.splice(index, 1);
-    } 
+      this.items.splice(index, 1);
+    }
+  }
+
+  async addItem() {
+    const alert = await this.alertController.create({
+      subHeader: 'Add Item',
+      message: 'Please add an item.',
+      inputs: [
+        {
+          name: 'itemName',
+          type: 'text',
+          placeholder: 'Name of item, e.g. "apples"'
+        },
+        {
+          name: 'itemType',
+          type: 'text',
+          placeholder: 'Type of item, e.g. "granny smith'
+        }],
+      buttons: [{
+        text: "Add",
+        handler: data => {
+          this.items.push({ Name: data.itemName, Type: data.itemType, src: "/assets/404.jpg", purchased: false })
+        }
+      }, 
+      {
+        text: "Cancel"
+      }]
+    });
+
+    alert.present();
   }
 }
